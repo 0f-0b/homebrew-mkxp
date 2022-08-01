a65f604419457a421d19af79acc632551f7d2e11
undefined <sjx233@qq.com>
2022-08-02 02:22:46 +0800

    rgss-script-editor 0+20201020

---
+++ Formula/rgss-script-editor.rb
@@ -0,0 +1,25 @@
+class RgssScriptEditor < Formula
+  desc "Editor for RPGMaker script archives"
+  homepage "https://github.com/Ancurio/rgss_script_editor"
+  url "https://github.com/Ancurio/rgss_script_editor.git",
+    revision: "e7483ee191a6823d32b3df0694392a45ccacfe5d"
+  version "0+20201020"
+  head "https://github.com/Ancurio/rgss_script_editor.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "qscintilla2"
+  depends_on "qt@5"
+
+  def install
+    ENV.cxx11
+
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    bin.install "bin/rgss_script_editor"
+    bin.install "bin/rgss_script_editor_cli"
+  end
+
+  test do
+    system "true"
+  end
+end
