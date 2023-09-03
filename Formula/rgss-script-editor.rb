50453bf129c3feaf0199cc1ff2a193cc1e983f01
ud2 <sjx233@qq.com>
2023-09-03 19:42:54 +0800

    build qscintilla from source

--- Formula/rgss-script-editor.rb
+++ Formula/rgss-script-editor.rb
@@ -7,13 +7,47 @@ class RgssScriptEditor < Formula
   head "https://github.com/Ancurio/rgss_script_editor.git", branch: "master"
 
   depends_on "cmake" => :build
-  depends_on "qscintilla2"
   depends_on "qt@5"
+
+  resource "qscintilla2" do
+    url "https://riverbankcomputing.com/static/Downloads/QScintilla/2.14.1/QScintilla_src-2.14.1.tar.gz"
+    sha256 "dfe13c6acc9d85dfcba76ccc8061e71a223957a6c02f3c343b30a9d43a4cdd4d"
+  end
+
+  patch :DATA
 
   def install
     ENV.cxx11
 
-    system "cmake", ".", *std_cmake_args
+    qsci = libexec/"qscintilla2"
+    qsci.mkpath
+
+    resource("qscintilla2").stage do
+      qt5 = Formula["qt@5"].opt_prefix
+
+      cd "src" do
+        inreplace "qscintilla.pro" do |s|
+          s.gsub! "QMAKE_POST_LINK += install_name_tool -id @rpath/$(TARGET1) $(TARGET)",
+            "QMAKE_POST_LINK += install_name_tool -id #{qsci}/lib/$(TARGET1) $(TARGET)"
+          s.gsub! "$$[QT_INSTALL_LIBS]", qsci/"lib"
+          s.gsub! "$$[QT_INSTALL_HEADERS]", qsci/"include"
+          s.gsub! "$$[QT_INSTALL_TRANSLATIONS]", qsci/"trans"
+          s.gsub! "$$[QT_INSTALL_DATA]", qsci/"data"
+          s.gsub! "$$[QT_HOST_DATA]", qsci/"data"
+        end
+
+        inreplace "features/qscintilla2.prf" do |s|
+          s.gsub! "$$[QT_INSTALL_LIBS]", qsci/"lib"
+          s.gsub! "$$[QT_INSTALL_HEADERS]", qsci/"include"
+        end
+
+        system "#{qt5}/bin/qmake"
+        system "make"
+        system "make", "install"
+      end
+    end
+
+    system "cmake", ".", *std_cmake_args, "-DCMAKE_PREFIX_PATH=#{qsci}"
     system "make"
     bin.install "bin/rgss_script_editor"
     bin.install "bin/rgss_script_editor_cli"
@@ -25,3 +59,17 @@ class RgssScriptEditor < Formula
     system bin/"rgss_script_editor_cli", ".", "Scripts.rvdata2"
   end
 end
+__END__
+diff --git a/Modules/FindQScintilla.cmake b/Modules/FindQScintilla.cmake
+index b3d79bd..d7882ae 100644
+--- a/Modules/FindQScintilla.cmake
++++ b/Modules/FindQScintilla.cmake
+@@ -15,7 +15,7 @@ if(Qt5Core_FOUND)
+
+   if(QSCINTILLA_INCLUDE_INTERNAL AND QSCINTILLA_LIBRARY)
+     set(QScintilla_FOUND TRUE)
+-    set(QSCINTILLA_INCLUDE_DIRS "${QSCINTILLA_INCLUDE_INTERNAL}/Qsci")
++    set(QSCINTILLA_INCLUDE_DIRS "${QSCINTILLA_INCLUDE_INTERNAL}")
+     message(STATUS "QScintilla found: ${QSCINTILLA_LIBRARY}")
+   else()
+     set(QScintilla_FOUND FALSE)
