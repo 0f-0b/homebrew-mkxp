bd4c5922373af6b5f597762bd0912ed440aa6761
ud2 <sjx233@qq.com>
2025-02-23 13:35:15 +0800

    align marshal representation with mri

--- Formula/rgss-script-editor.rb
+++ Formula/rgss-script-editor.rb
@@ -73,3 +73,110 @@ index b3d79bd..d7882ae 100644
      message(STATUS "QScintilla found: ${QSCINTILLA_LIBRARY}")
    else()
      set(QScintilla_FOUND FALSE)
+diff --git a/src/ruby_data.cxx b/src/ruby_data.cxx
+index 4eb2786..fe0063c 100644
+--- a/src/ruby_data.cxx
++++ b/src/ruby_data.cxx
+@@ -266,22 +266,22 @@ static void writeFixnum(QIODevice &dev, int value)
+
+   if (value > 0) {
+     /* Positive number */
+-    if (value <= 0x7F)
++    if (value <= 0xFF)
+       len = 1;
+-    else if (value <= 0x7FFF)
++    else if (value <= 0xFFFF)
+       len = 2;
+-    else if (value <= 0x7FFFFF)
++    else if (value <= 0xFFFFFF)
+       len = 3;
+     else
+       len = 4;
+   }
+   else {
+     /* Negative number */
+-    if (value >= (int) 0x80)
++    if (value >= ~0xFF)
+       len = -1;
+-    else if (value >= (int) 0x8000)
++    else if (value >= ~0xFFFF)
+       len = -2;
+-    else if (value <= (int) 0x800000)
++    else if (value >= ~0xFFFFFF)
+       len = -3;
+     else
+       len = -4;
+@@ -308,7 +308,7 @@ static void writeString(QIODevice &dev, const QByteArray &data)
+     throw QByteArray("Error writing data");
+ }
+
+-static void writeIVARString(QIODevice &dev, const QByteArray &data)
++static void writeIVARString(QIODevice &dev, const QByteArray &data, bool first)
+ {
+   /* Write inner string */
+   writeByte(dev, '"');
+@@ -318,14 +318,19 @@ static void writeIVARString(QIODevice &dev, const QByteArray &data)
+   writeFixnum(dev, 1);
+   // XXX It's no big deal, but maybe we should symlink all
+   // further references to ':E' as Ruby would do?
+-  writeByte(dev, ':');
+-  writeString(dev, "E");
++  if (first) {
++    writeByte(dev, ':');
++    writeString(dev, "E");
++  } else {
++    writeByte(dev, ';');
++    writeByte(dev, 0);
++  }
+   /* Always write Utf8 encoding */
+   writeByte(dev, 'T');
+ }
+
+ static void writeRubyString(QIODevice &dev, const QByteArray &data,
+-                            Script::Format format)
++                            Script::Format format, bool first)
+ {
+   if (format == Script::XP) {
+     writeByte(dev, '"');
+@@ -333,12 +338,12 @@ static void writeRubyString(QIODevice &dev, const QByteArray &data,
+   }
+   else { /* format == ScriptArchive::VXAce */
+     writeByte(dev, 'I');
+-    writeIVARString(dev, data);
++    writeIVARString(dev, data, first);
+   }
+ }
+
+ static void writeScript(QIODevice &dev, const Script &script,
+-                        Script::Format format)
++                        Script::Format format, bool first)
+ {
+   /* Write array prologue */
+   writeByte(dev, '[');
+@@ -349,7 +354,7 @@ static void writeScript(QIODevice &dev, const Script &script,
+   writeFixnum(dev, script.magic);
+
+   /* Write name */
+-  writeRubyString(dev, script.name.toUtf8(), format);
++  writeRubyString(dev, script.name.toUtf8(), format, first);
+
+   /* Convert line endings: Unix -> Windows (for compat) */
+   QString sdata;
+@@ -368,7 +373,7 @@ static void writeScript(QIODevice &dev, const Script &script,
+   /* Write script data */
+   QByteArray data = sdata.toUtf8();
+   data = compressData(data);
+-  writeRubyString(dev, data, format);
++  writeRubyString(dev, data, format, false);
+ }
+
+ ScriptList readScripts(QIODevice &dev)
+@@ -407,7 +412,7 @@ void writeScripts(const ScriptList &scripts,
+
+   /* Write scripts */
+   for (int i = 0; i < scripts.count(); ++i)
+-    writeScript(dev, scripts[i], format);
++    writeScript(dev, scripts[i], format, i == 0);
+ }
+
+ int generateMagic(const ScriptList &scripts)
